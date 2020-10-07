6fe24482f2a39ae3894a3412fc9e3ed5670c06ce
joshua kim <j0shxone@gmail.com>
2020-10-07 16:15:29 -0400

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cryptos: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR')
+      .then(res => {
+        const cryptos = res.data;
+        console.log(cryptos);
+        this.setState({cryptos: cryptos});
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {Object.keys(this.state.cryptos).map((key) => (
+          <div id="crypto-container">
+            <span className="left">{key}</span>
+            <span className="right">${this.state.cryptos[key].USD}</span>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
